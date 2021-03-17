08b4ce9be41c9e4ee7d71c1c94dfbe0bf5393739
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-18 02:47:58 +0500

    add custom buttoon and export it from common components

---
+++ src/components/common/Button/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Button } from "antd";
+
+export const StyledButton = styled(Button)`
+  width: 100%;
+  min-height: 40px;
+  border-radius: 1rem;
+  border: none;
+  box-shadow: 1px 1px 1px 1px rgba(211, 194, 194, 0.25);
+`;
