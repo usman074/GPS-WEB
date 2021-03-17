ec836400e04cd0523ac9d36914300099e2707f00
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-18 02:48:40 +0500

    add login screen

---
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { Button, Input } from "../common";
+import { LoginWrapper, LoginContainer } from "./style";
+export const LoginScreen = () => {
+  const validateLoginForm = () => {
+    return Yup.object({
+      password: Yup.string()
+        .min(8, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    });
+  };
+
+  const handleLoginForm = (values) => {
+    console.log(values);
+  };
+  return (
+    <LoginWrapper>
+      <LoginContainer>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validateLoginForm}
+          onSubmit={handleLoginForm}
+        >
+          <Form>
+            <h1 className="page-heading">Welcome To GPS Web</h1>
+            <h1 className="form-heading">Sign In</h1>
+
+            <p className="label">Email</p>
+            <Input name="email" placeholder="hello@Sample.com" type="text" />
+
+            <p className="label">Password</p>
+            <Input
+              name="password"
+              type="password"
+              placeholder="Enter Password"
+            />
+
+            <Button className="login-button" type="submit" name={"Sign In"} />
+          </Form>
+        </Formik>
+      </LoginContainer>
+    </LoginWrapper>
+  );
+};
---
+++ src/components/LoginScreen/style.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+  /* background: red; */
+  background-image: url("https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-600w-1725825019.jpg");
+  background-repeat: no-repeat, repeat;
+  background-position: center;
+  background-size: 100% 100%; 
+`;
+export const LoginContainer = styled.div`
+  /* background: red; */
+  /* background-image: url("https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-600w-1725825019.jpg");
+  background-repeat: no-repeat, repeat;
+  background-position: center;
+  background-size: cover; */
+  /* color: white; */
+  /* background-image: url("../../assets/BACKGROUND_IMAGE.png"); */
+
+  display: grid;
+  justify-content:center;
+  height: 100vh;
+
+  form {
+    max-width: 490px;
+    min-width: 300px;
+  }
+  h1.page-heading {
+    color: #ffffff;
+    font-size: 4rem;
+    font-weight: 700;
+    margin-bottom: 2rem !important;
+    margin-top: 2rem !important;
+    max-width: 30rem;
+  }
+  h1.form-heading {
+    color: #ffffff;
+    text-align: left;
+    font-size: 2.6rem;
+    margin-bottom: 2rem !important;
+  }
+  p.label {
+    color: #ffffff;
+    text-align: left;
+    font-size: 1.7rem;
+    font-weight: 700;
+    margin-bottom: 0.5rem !important;
+  }
+
+  .login-button {
+    max-width: 130px;
+    background: rgba(70, 70, 70, 0.5);
+
+    span {
+      font-size: 1.7rem;
+      font-weight: 700;
+    }
+  }
+`;
