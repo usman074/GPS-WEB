19830965e380f9ac929d250cb2682537738aae2a
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-18 02:49:21 +0500

    reder login screen and add antd style import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Components
-
+import {LoginScreen} from './components/LoginScreen/LoginScreen';
 //User Provider
 
 //External Libs
@@ -11,24 +11,12 @@ import { Route, Redirect, Switch } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginScreen />
     </div>
   );
 }
