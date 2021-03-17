c36a7e9a65b268b11b4375169aa34eb69b7b71af
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-18 02:47:14 +0500

    add custom input

---
+++ src/components/common/Input/Input.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { StyledInput, InputWrapper } from "./style";
+import { useField } from "formik";
+
+// customStyle={
+//     height: 'value'
+//     border: 'value'
+//     borderBottom: 'value'
+//     placeHolderColor: 'value'
+//     errorColor: 'value'
+// }
+
+export const Input = ({
+  placeholder,
+  customStyle,
+  type = "text",
+  disabled = false,
+  className,
+  mainClassName = "",
+  ...props
+}) => {
+  const [field, meta] = useField(props);
+  return (
+    <InputWrapper>
+      <StyledInput
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        disabled={disabled}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <p className="error">{meta.error}</p>
+      ) : null}
+    </InputWrapper>
+  );
+};
---
+++ src/components/common/Input/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Input } from "antd";
+
+export const StyledInput = styled(Input)`
+  width: 100%;
+  border-radius: 0.7rem;
+  min-height: 4rem;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+  &::placeholder {
+    color: #6C6C6C;
+  }
+`;
+
+export const InputWrapper = styled.div`
+    margin-bottom: 2rem;
+  .error {
+    text-align: left;
+    color: red !important;
+  }
+`;
