f237ef5a84724ea9795eaf208c52b6dafe050c45
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-18 20:36:04 +0500

    change default border radius for button

--- src/components/common/Button/style.js
+++ src/components/common/Button/style.js
@@ -4,7 +4,7 @@ import { Button } from "antd";
 export const StyledButton = styled(Button)`
   width: 100%;
   height: 40px;
-  border-radius: 1rem;
+  border-radius: 0.5rem;
   border: none;
   box-shadow: 1px 1px 1px 1px rgba(211, 194, 194, 0.25);
 `;
