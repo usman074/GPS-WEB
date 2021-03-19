d7e4eee91f527163f825b22177e697c9a30290ac
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-19 15:17:52 +0500

    add dashboard

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export const Dashboard = ()=>{
+    console.log("helo");
+    return (
+     <img style={{width: '100%'}} src={'staticmap.png'} />
+    );
+  }
