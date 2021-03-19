d563f538bb44f67960e96a9c53e8bfcfda5b7ce1
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-19 12:38:01 +0500

    make layout dynamic

--- src/layoutStyle.js
+++ src/layoutStyle.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
     display: grid;
-    grid-template-columns: 256px 1fr;
+    grid-template-columns: ${props=> props.isLogin? '256px 1fr': '1fr'};
     grid-template-rows: auto 1fr;
 
-    grid-template-areas: "sidemenu header" "sidemenu content";
+    grid-template-areas: ${props=> props.isLogin? '"sidemenu header" "sidemenu content"': 'none'};
 `;
 
 export const HeaderWrapper = styled.div`
