436fd544b2bf2a31f0c5e5505ead5bb86797d373
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-19 12:39:11 +0500

    add events and dynamic active class

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -3,14 +3,23 @@ import { Button } from "../index";
 import { HeaderWrapper } from "./style";
 import { UploadOutlined } from "@ant-design/icons";
 import { Space } from "antd";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Header = () => {
+  const location = useLocation();
+  const {pathname} = location;
+  const history = useHistory()
+  const changeScreen = (name)=> {
+    console.log(name)
+    history.replace(name)
+  }
+  
   return (
     <HeaderWrapper>
       <Space size={90}>
-        <Button name="Dashboard" className="header-buttons active" />
-        <Button name="Interval" className="header-buttons" />
-        <Button name="Settings" className="header-buttons" />
+        <Button name="Dashboard" className={`header-buttons ${pathname === '/dashboard'? 'active': ''}`}  clickEvent={()=> changeScreen('/dashboard')} />
+        <Button name="Interval" className={`header-buttons ${pathname === '/interval'? 'active': ''}`} clickEvent={()=> changeScreen('/interval')}/>
+        <Button name="Settings" className={`header-buttons ${pathname === '/settings/user' || pathname === '/settings/language' || pathname === '/settings/terms'? 'active': ''}`} clickEvent={()=> changeScreen('/settings/user')}/>
       </Space>
       <span>
         <UploadOutlined style={{color: 'red'}} /> <p>Log Out</p>
--- src/components/common/SettingsLeftPanel/SettingsLeftPanel.jsx
+++ src/components/common/SettingsLeftPanel/SettingsLeftPanel.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Button } from "../index";
 import { Space } from "antd";
 import {ContentWrapper} from './style'
+import { useHistory, useLocation } from "react-router-dom";
 
 
 export const SettingsLeftPanel = ({gridArea}) => {
+  const history = useHistory()
+  const location = useLocation();
+  const {pathname} = location;
+
   return (
     <ContentWrapper gridArea={gridArea}>
-      <Button name="Create User" className="settings-buttons active" />
-      <Button name="Change Language" className="settings-buttons" />
-      <Button name="Update Term & Condition" className="settings-buttons" />
+      <Button name="Create User" className={`settings-buttons ${pathname === '/settings/user'? 'active': ''}`} clickEvent={()=> history.replace('/settings/user')} />
+      <Button name="Change Language" className={`settings-buttons ${pathname === '/settings/language'? 'active': ''}`} clickEvent={()=> history.replace('/settings/language')} />
+      <Button name="Update Term & Condition" className={`settings-buttons ${pathname === '/settings/terms'? 'active': ''}`} clickEvent={()=> history.replace('/settings/terms')} />
     </ContentWrapper>
   );
 };
