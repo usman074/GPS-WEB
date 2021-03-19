7e8e6a22be58ecf2316dd2dd4d493e43d78aed99
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-19 12:39:21 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //Components
 import { LoginScreen } from "./components/LoginScreen/LoginScreen";
+import { CreateUserScreen } from "./components/CreateUserScreen/CreateUserScreen";
 import { Sidemenu, Header } from "./components/common";
 //User Provider
 
@@ -13,19 +14,42 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
-import { AppContainer, HeaderWrapper, SidemenuWrapper } from "./layoutStyle";
+import {
+  AppContainer,
+  HeaderWrapper,
+  SidemenuWrapper,
+  ContentWrapper,
+} from "./layoutStyle";
 
 function App() {
+  const  [isLogin, setIsLogin] = useState(false);
   return (
     <div className="App">
       {/* <LoginScreen /> */}
-      <AppContainer>
+      <AppContainer isLogin={isLogin}>
         <SidemenuWrapper>
-          <Sidemenu />
+          <Sidemenu isLogin={isLogin} />
         </SidemenuWrapper>
-        <HeaderWrapper>
+        {isLogin && <HeaderWrapper>
           <Header />
-        </HeaderWrapper>
+        </HeaderWrapper>}
+        {/* <ContentWrapper>
+          <CreateUserScreen />
+        </ContentWrapper> */}
+        <Switch>
+          <Route path="/login">
+            <LoginScreen isLogin={isLogin} setIsLogin={setIsLogin} />
+          </Route>
+          <ContentWrapper>
+            {/* <Route path="/dashboard">
+              <CreateUserScreen />
+            </Route> */}
+            <Route path="/settings/user">
+              <CreateUserScreen />
+            </Route>
+          </ContentWrapper>
+          <Redirect from="/" exact to="/login"></Redirect>
+        </Switch>
       </AppContainer>
     </div>
   );
