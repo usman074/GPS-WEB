c92f80292f5eab2e2adf409c169acb542ad11f97
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-03-31 21:40:55 +0500

    add auth and user  provider in app js and refresh login

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 //Components
 import { LoginScreen } from "./components/LoginScreen/LoginScreen";
 import { SettingsScreen } from "./components/SettingsScreen/SettingsScreen";
-import {IntervalScreen} from './components/IntervalScreen/IntervalScreen'
-import {Dashboard} from './components/Dashboard/Dashboard'
-import { Sidemenu, Header } from "./components/common";
-//User Provider
+import { IntervalScreen } from "./components/IntervalScreen/IntervalScreen";
+import { Dashboard } from "./components/Dashboard/Dashboard";
+import {
+  Sidemenu,
+  Header,
+  ProtectedRoute,
+  SettingUserContext,
+} from "./components/common";
+//Provider
+import { AuthProvider } from "./providers/AuthProvider";
+import { UserProvider } from "./providers/UserProvider";
 
 //External Libs
 import { Route, Redirect, Switch } from "react-router-dom";
@@ -25,6 +32,13 @@ import {
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
+  const token = localStorage.getItem("uid");
+
+  useEffect(()=> {
+    if (token) {
+      setIsLogin(true)
+    }
+  }, token)
   return (
     <div className="App">
       <AppContainer isLogin={isLogin}>
@@ -32,33 +46,43 @@ function App() {
           <Sidemenu isLogin={isLogin} />
         </SidemenuWrapper>
         {isLogin && (
-          <HeaderWrapper >
-            <Header setIsLogin={setIsLogin}/>
+          <HeaderWrapper>
+            <Header setIsLogin={setIsLogin} />
           </HeaderWrapper>
         )}
-        <Switch>
-          <ContentWrapper isLogin={isLogin}>
-            <Route path="/login">
-              <LoginScreen isLogin={isLogin} setIsLogin={setIsLogin} />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/interval">
-              <IntervalScreen />
-            </Route>
-            <Route path="/settings/user">
-              <SettingsScreen />
-            </Route>
-            <Route path="/settings/language">
-              <SettingsScreen />
-            </Route>
-            <Route path="/settings/terms">
-              <SettingsScreen />
-            </Route>
-            <Redirect from="/" exact to="/login"></Redirect>
-          </ContentWrapper>
-        </Switch>
+        <AuthProvider>
+          <SettingUserContext setIsLogin={setIsLogin} />
+          <Switch>
+            <ContentWrapper isLogin={isLogin}>
+              <ProtectedRoute
+                path="/dashboard"
+                component={Dashboard}
+              ></ProtectedRoute>
+              <ProtectedRoute
+                path="/interval"
+                component={IntervalScreen}
+              ></ProtectedRoute>
+              <UserProvider>
+                <ProtectedRoute
+                  path="/settings/user"
+                  component={SettingsScreen}
+                ></ProtectedRoute>
+              </UserProvider>
+              <ProtectedRoute
+                path="/settings/language"
+                component={SettingsScreen}
+              ></ProtectedRoute>
+              <ProtectedRoute
+                path="/settings/terms"
+                component={SettingsScreen}
+              ></ProtectedRoute>
+              <Route path="/login">
+                <LoginScreen isLogin={isLogin} setIsLogin={setIsLogin} />
+              </Route>
+              {!token && <Redirect from="/" exact to="/login"></Redirect>}
+            </ContentWrapper>
+          </Switch>
+        </AuthProvider>
       </AppContainer>
     </div>
   );
