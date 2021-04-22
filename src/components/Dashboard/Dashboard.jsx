6ee032daba81b91e9edfc10088ae0dabb73706ea
Usman Iqbal <usman.iqbal6867@gmail.com>
2021-04-23 03:07:29 +0500

    update dashboard to show multiple locations on a single map

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,48 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useVehicleContext } from "../../providers/VehicleProvider";
-import { useAuthContext } from "../../providers/AuthProvider";
-import GoogleMapReact from 'google-map-react';
-import {MarkerIcon} from '../../assets/index';
-import {CarNameStyled} from './style';
+import GoogleMapReact, {fitBounds} from "google-map-react";
+import { MarkerIcon } from "../../assets/index";
+import { CarNameStyled } from "./style";
 
 const AnyReactComponent = ({ text }) => {
   return (
     <div>
-      <MarkerIcon/>
+      <MarkerIcon />
       {text && <CarNameStyled>{text}</CarNameStyled>}
     </div>
-  )
-}
+  );
+};
 
 export const Dashboard = () => {
   const { state } = useVehicleContext();
-
-
-// const loadStaticMap = ()=> {
-//   if (state.selectedVehicle) {
-//     const {lat, lng} = state.selectedVehicle;
-//     const mapKey = 'AIzaSyC60nn_VZXJTxtmWuIB9SnHGU6USDb1LGo';
-//     let url = 'https://maps.googleapis.com/maps/api/staticmap?center='
-//       + lat + ',' + lng + '&zoom=20&size=1000x500&markers=color:red|' + lat
-//       + ',' + lng + '&key=' + mapKey;
-//     return url;
-//   }
-// }
-
-  console.log(state)
+  const [selectedVehicles, setSelectedVehicles] = useState([]);
+  const [centerCoord, setCenterCoord] = useState({lat: 0, lng: 0});
+  useEffect(() => {
+    const filterVehicles = state.vehicles.filter((vehicle) =>
+      state.selectedVehicle.includes(vehicle.uid)
+    );
+    const latVal = filterVehicles.length && filterVehicles.reduce((acc, curr)=> acc + curr.lat
+    ,0)
+    const lngVal = filterVehicles.length && filterVehicles.reduce((acc, curr)=> acc + curr.lng, 0)
+    {filterVehicles.length && setCenterCoord({lat: latVal/filterVehicles.length, lng: lngVal/filterVehicles.length })}
+    setSelectedVehicles(filterVehicles);
+  }, [state]);
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCQkNEL5ocv5x_TfGqgXW-olHr0yeA0N0Q' }}
-          center={{lat: state.selectedVehicle? state.selectedVehicle.lat: 30.7438368, lng: state.selectedVehicle? state.selectedVehicle.lng: 73.3308934}}
-          defaultZoom={11}
-        >
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCQkNEL5ocv5x_TfGqgXW-olHr0yeA0N0Q" }}
+        center={{
+          lat:  selectedVehicles.length? centerCoord.lat: 30.7438368,
+          lng: selectedVehicles.length? centerCoord.lng: 73.3308934,
+        }}
+        defaultZoom={0}
+      >
+        {selectedVehicles.map((vehicle) => (
           <AnyReactComponent
-            lat={state.selectedVehicle? state.selectedVehicle.lat: 30.7438368}
-            lng={state.selectedVehicle? state.selectedVehicle.lng: 73.3308934}
-            text={state.selectedVehicle && state.selectedVehicle.vehicleName}
+            key={vehicle.uid}
+            lat={vehicle.lat}
+            lng={vehicle.lng}
+            text={vehicle && vehicle.vehicleName}
           />
-        </GoogleMapReact>
-      </div>
-  )
+        ))}
+        { selectedVehicles.length === 0 && <AnyReactComponent
+          lat={30.7438368}
+          lng={73.3308934}
+        />}
+      </GoogleMapReact>
+    </div>
+  );
 };
